9941afdbba7bc60435eaeaf0d1726478b257f6cf
Nescabir <boulongne.bapt@gmail.com>
2019-04-12 11:59:05 +0200

    Recup Joueurs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
+  require "httparty"
+
+  getToken = HTTParty.get('http://193.70.0.15:8080/tokens/log/technicien/technicien2018*')
+  token = JSON.parse getToken.body
+  token = token['token']
+  $bearerToken = 'Bearer ' + token
+
   def home
+
+    getJoueursMethod()
   end
+
+  def getJoueursMethod
+    getJoueurs = HTTParty.get('http://193.70.0.15:8080/joueurs', :headers => {
+      "Authorization" => $bearerToken
+    })
+    @joueurs = JSON.parse getJoueurs.body
+  end
+  
 end
